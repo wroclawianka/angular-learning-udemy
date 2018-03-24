40fb018c9bb39eb92ff8ac4923c2bc287e03dea1
wroclawianka <dorota.zelga@gmail.com>
2018-03-24 13:03:33 +0100

    encalpsulation desccribed

--- basics-assignment-all/src/app/components/server-console-2/server-element/server-element.component.ts
+++ basics-assignment-all/src/app/components/server-console-2/server-element/server-element.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.None, 
+  // Emulated - default 
+  // None - means that even if you put style in this component, then will be use in all places (application wide)
+  // Native -  shadow DOM technology, only in browsers that support it, allows hidden DOM trees to be attached to elements in the regular DOM tree
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
