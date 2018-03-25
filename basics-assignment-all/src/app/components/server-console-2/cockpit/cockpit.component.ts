08bb4a26fc91da43ae32494b3a8a5a18a1985741
wroclawianka <dorota.zelga@gmail.com>
2018-03-25 12:48:38 +0200

    getting access to template and DOM

--- basics-assignment-all/src/app/components/server-console-2/cockpit/cockpit.component.ts
+++ basics-assignment-all/src/app/components/server-console-2/cockpit/cockpit.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css',
+  '../../../../styles/answer.css',]
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>(); //EventEmitter - TS generic type
   @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>(); //alias
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  
+  //@ViewChild('selector') you can also access here component by @ViewChild(CockpitComponent)
+  @ViewChild('serverContentInput') serverContentInput: ElementRef; 
 
   constructor() { }
-
-  ngOnInit() {
+  
+  //ngOnInit - a lifecycle hook called when tha comp. is initialized 
+  //More: ngOnChanges - bound input property changes, ngDoCheck - on every chanage detection, 
+  //ngAfterContentInit, ngAfterContentChecked, ngAfterViewInit, ngAfterViewChecked
+  ngOnInit() { 
   }
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent}); // will emit a new event of this type using EventEmitter
+  onAddServer(nameInput : HTMLInputElement) {
+    // this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent}); // will emit a new event of this type using EventEmitter
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value}); // will emit a new event of this type using EventEmitter
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({blueprintName: this.newServerName, blueprintContent: this.newServerContent});
+  onAddBlueprint(nameInput : HTMLInputElement) {
+    // this.blueprintCreated.emit({blueprintName: this.newServerName, blueprintContent: this.newServerContent});
+    this.blueprintCreated.emit({blueprintName: nameInput.value, blueprintContent: this.serverContentInput.nativeElement.value});
   }
 }
