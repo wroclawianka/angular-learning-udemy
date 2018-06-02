a28195cb7e97e93577568f95c4c35221f38917fa
wroclawianka <dorota.zelga@gmail.com>
2018-06-02 14:33:05 +0200

    Host Binding used

--- src/app/components/directives/better-highlight/better-highlight.directive.ts
+++ src/app/components/directives/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,26 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  // () - to which property of the emelent you want to bind
+  // Camel case is important here as we accessing DOM property which does not know dashes
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue') 
+    // Renderer in not wrong usage, but not more needed in this example 
+    this.backgroundColor = 'blue';   
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')  
+    this.backgroundColor = 'transparent';  
   }
 }
